d2e2c39f010778f6cfb5dbd148eb6b488e9cfd9f
Somtozech <ezechinnaemeka@gmail.com>
2021-08-30 19:35:31 +0100

    feat: Setup Hardhat configuration for deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 
+require("dotenv").config()
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +20,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL, 
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    }
+  }
 };
