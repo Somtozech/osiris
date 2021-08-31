a800d6939c680419822a5b938eaccc51255e130a
Somtozech <ezechinnaemeka@gmail.com>
2021-08-31 11:22:13 +0100

    feat: Setup UI for upload in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,61 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState, useCallback, useMemo } from "react";
+import { useDropzone } from "react-dropzone";
+import Icon from "./components/Icon";
+import { acceptStyle, baseStyle, rejectStyle, activeStyle } from "./styles";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [buffer, setBuffer] = useState("");
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+
+    const reader = new FileReader();
+
+    reader.onabort = () => console.log("file reading was aborted");
+    reader.onerror = () => console.log("file reading has failed");
+    reader.onload = () => {
+      setBuffer(reader.result);
+    };
+    reader.readAsArrayBuffer(file);
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({ accept: "image/*", maxFiles: 1, onDrop });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  );
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="container">
+      <section className="upload-section">
+        <h1>Create Item</h1>
+        <div {...getRootProps({ style })}>
+          <input {...getInputProps()} />
+          <Icon width="64" height="64" />
+          <p>
+            Drop Files here or <span style={{ color: "#11DBBC" }}> Browse</span>
+          </p>
+        </div>
+        <div className="center">
+          <button>Upload</button>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
